317ea8ca2960a4c01d0aea82a035fa435ceac014
Pablo Araujo <pabloaraujogalo@gmail.com>
2021-01-19 19:46:47 -0300

    Finalizando Programa Principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,9 +1,62 @@
 package application;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Departament;
+import entities.HourContract;
+import entities.Worker;
+import entities.enuns.WorkerLevel;
+
 public class Program {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) throws ParseException {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		SimpleDateFormat sdf = new SimpleDateFormat ("dd/MM/yyyy");
+		
+		System.out.print("Enter department's name: ");
+		String departamentName = sc.nextLine();
+		System.out.println("Enter worker data: ");
+		System.out.print("Name: ");
+		String workerName = sc.nextLine();
+		System.out.print("Level: ");
+		String workerLevel = sc.nextLine();
+		System.out.print("Base salary: ");
+		double workerBaseSalary = sc.nextDouble();
+		
+		Worker worker = new Worker(workerName,WorkerLevel.valueOf(workerLevel),workerBaseSalary,new Departament(departamentName));
+		
+		System.out.print("How many contracts to this worker?");
+		int n = sc.nextInt();
+		
+		for(int i=0;i<n;i++) {
+			System.out.println("Enter contract #" + (i+1) + " data:");
+			System.out.print("Date (DD/MM/YYYY): ");
+			Date contractDate = sdf.parse(sc.next()); 
+			System.out.print("Value per hour: ");
+			double valuePerHour = sc.nextDouble();
+			System.out.print("Duration (hours): ");
+			int hours = sc.nextInt();
+			
+			HourContract contract = new HourContract(contractDate, valuePerHour, hours);
+			worker.addContract(contract);
+		}
+		
+		System.out.print("Enter month and year to calculate income (MM/YYYY): ");
+		sc.nextLine();
+		String monthAndYear = sc.nextLine();
+		int month = Integer.parseInt(monthAndYear.substring(0,2));
+		int year = Integer.parseInt(monthAndYear.substring(3));
+		System.out.println("Name: " + worker.getName());
+		System.out.println("Departament: " + worker.getDepartament().getName());
+		System.out.println("Income for 08/2018: " + String.format("%.2f", worker.income(year, month)));
+		
+		sc.close();
 
 	}
 
